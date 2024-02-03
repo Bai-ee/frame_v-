d33911d2a56ebf41c1490985aa542c04fb87bbb7
Bryan  Balli <bryanballi@bryans-mac-mini.lan>
2024-02-02 23:45:13 -0600

    WIP

---
+++ et004-main/track.js
@@ -0,0 +1,114 @@
+const trackDir = "";
+const bpm = 122.06;
+const parts = [
+    { file: "1.wav", length: 8, loop: 1 },
+    { file: "2.wav", length: 8, loop: 0 },
+    { file: "4.wav", length: 8, loop: 0 },
+    { file: "5.wav", length: 4, loop: 0 },
+    { file: "4.wav", length: 8, loop: 0 },
+    { file: "5.wav", length: 4, loop: 0 },
+    { file: "6.wav", length: 8, loop: 0 },
+    { file: "5.wav", length: 4, loop: 0 },
+    { file: "6.wav", length: 8, loop: 0 },
+    { file: "4.wav", length: 8, loop: 0 },
+    { file: "2.wav", length: 8, loop: 0 },
+    { file: "1.wav", length: 8, loop: 1 },
+    // { file: "8.wav", length: 4, loop: 0 }, 
+    // { file: "1.wav", length: 4, loop: 1 },
+];
+const presets = [];
+presets.push([    
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    2,
+    1,
+    2,
+    2,
+    2,
+    2,
+
+]);
+presets.push([
+
+
+    2,
+    2,
+    2,
+    2,
+    0,
+    0,
+    4,
+    2,
+    4,
+    4,
+    4,
+    4,
+
+]);
+presets.push([ 
+
+    0,
+    1,
+    1,
+    1,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+
+
+]);
+presets.push([
+    0,0,0,0,0,
+    0,0,0,0,0,
+    0,0,0,0,0,
+    0,0,0,0,0,
+    0,0,0,0,0,
+    0,0,0,0,0,
+    0,0,0,0,0
+]);
+presets.push([
+    0,0,0,0,0,
+
+    0,0,0,0,0,
+    0,0,0,0,0,
+    0,0,0,0,0,
+    0,0,0,0,0,
+    0,0,0,0,0,
+    0,0,0,0,0
+]);
+presets.push([
+    0,0,0,0,0,
+    0,0,0,0,0,
+    0,0,0,0,0,
+    0,0,0,0,0,
+    0,0,0,0,0,
+    0,0,0,0,0,
+    0,0,0,0,0
+]);
+
+// console.log("PRODUCER: SLUM GOD")
+// console.log("LABEL: Housepit CHI | HPIT.info")
+// console.log("DEVELOPER: Bryan (Bai-ee) Balli")
+// console.log("ABOUT: EditTrax is a collectable digital audio workstation NFT that allows for the non-destructive editing, rendering, and direct downloading of audio .wav files.")
+// console.log("LIMITATIONS:")
+
+// console.log("• DOWNLOADS ENABLED IN SAFARI BROWSERS ONLY. MUST BE VIEWING ON HICETNUNC")
+// console.log("• Original artist retains all creative rights to any edit created and downloaded")
+// console.log("• You are fully encouraged to use downloaded edits in your mix tapes, social content creation and public performances")
+// console.log("• You are not allowed to distribute downloaded edits for direct sale or distribution in any way")
+// console.log("CONTACT: edittrax@protonmail.com")
+// console.log("WEB: EditTrax.nft | .com")
+
+
+const downloadName = "No_Mills_(Bai-ee_/_Secret_Studio).wav"
+const boxHeight = 50; //min height == 40 -> otherwise clickable buttons are to small
+const reverseScrolling = false;
